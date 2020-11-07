e2fc8d3d05817d2da3faf5afd5180acf7c0f6a05
Daniel Gold <contact@golddaniel.ca>
2020-11-07 01:14:37 -0600

    Added randomized trade UI

--- Assets/Scripts/ChatCollision.cs
+++ Assets/Scripts/ChatCollision.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatCollision : MonoBehaviour
 {
-    [SerializeField]
-    public CameraController camController; 
     
+    public CameraController camController;
+    
+    
+
+    public Sprite[] possibleSprites;
+
+    private int sellIndex;
+    private int buyIndex;
+
+    private GameObject display;
+
+
+    public void Start()
+    {
+        sellIndex = Random.Range(0, possibleSprites.Length);
+
+        buyIndex = sellIndex;
+        while(buyIndex == sellIndex)
+        {
+            buyIndex = Random.Range(0, possibleSprites.Length);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -15,6 +36,46 @@ public class ChatCollision : MonoBehaviour
             camController.targetOrthoSize = 6;
             camController.targetPosition = this.transform;
         }
+
+        display = new GameObject("Booth Text");
+        display.transform.parent = GameObject.Find("UI").transform.GetChild(1);
+        display.transform.localScale = new Vector3(1, 1, 1);
+
+
+        GameObject textGO = new GameObject("Title Text");
+        Text text = textGO.AddComponent<Text>();
+        text.rectTransform.SetParent(display.transform);
+        text.rectTransform.localPosition += new Vector3(6, 10, 0);
+        text.rectTransform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+        text.text = "I will trade";
+        text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+
+        GameObject buyingGO = new GameObject("Buying");
+        Image image = buyingGO.AddComponent<Image>();
+        image.rectTransform.SetParent(display.transform);
+        image.rectTransform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        image.rectTransform.localPosition += new Vector3(-16, 0, 0);
+        image.overrideSprite = possibleSprites[buyIndex];
+
+
+        GameObject forTextGO = new GameObject("Space Text");
+        text = forTextGO.AddComponent<Text>();
+        text.rectTransform.SetParent(display.transform);
+        text.rectTransform.localPosition += new Vector3(24, -16, 0);
+        text.rectTransform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+        text.text = "for";
+        text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+        GameObject sellingGO = new GameObject("Selling");
+        image = sellingGO.AddComponent<Image>();
+        image.rectTransform.SetParent(display.transform);
+        image.rectTransform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+        image.rectTransform.localPosition += new Vector3(16, 0, 0);
+        image.overrideSprite = possibleSprites[sellIndex];
+
+
+        display.transform.position = this.transform.position;
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -24,5 +85,8 @@ public class ChatCollision : MonoBehaviour
             camController.targetOrthoSize = 12;
             camController.targetPosition = collision.gameObject.transform;
         }
+
+        Destroy(display);
+        display = null;
     }
 }
