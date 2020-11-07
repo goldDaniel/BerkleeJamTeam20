30451749874399909ef5dc65dcaf326fd7399ddb
Daniel Gold <contact@golddaniel.ca>
2020-11-06 21:34:01 -0600

    Basic player movement in an arena. Simple follow-camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //the transform the camera is going to move towards
+    public Transform target;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //we use this because we do not want to alter the camera Z position
+        Vector3 finalPos = Vector3.Lerp(transform.position, target.position, 3 * Time.fixedDeltaTime);
+        finalPos.z = transform.position.z;
+        transform.position = finalPos;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Vector3 velocity;
+
+    public Rigidbody2D body;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        //units per seconds
+        float speed = 16;
+
+        velocity.Set(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x += 1;
+        }
+
+        velocity = velocity.normalized * speed;
+        body.velocity = velocity;
+    }
+}
