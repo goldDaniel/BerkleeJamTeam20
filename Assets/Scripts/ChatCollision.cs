8e957cb04d878b8be4580ad6e815c985eb597ac4
Daniel Gold <contact@golddaniel.ca>
2020-11-07 19:37:38 -0600

    Example changes

--- Assets/Scripts/ChatCollision.cs
+++ Assets/Scripts/ChatCollision.cs
@@ -32,53 +32,55 @@ public class ChatCollision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        player.SetBooth(booth);
-
         if(collision.gameObject.tag == "Player")
         {
+            player.SetBooth(booth);
+
+
             camController.targetOrthoSize = 6;
             camController.targetPosition = this.transform;
+            display = new GameObject("Booth Text");
+            display.transform.parent = GameObject.Find("UI").transform.GetChild(1);
+            display.transform.localScale = new Vector3(1, 1, 1);
+
+
+            GameObject textGO = new GameObject("Title Text");
+            Text text = textGO.AddComponent<Text>();
+            text.rectTransform.SetParent(display.transform);
+            text.rectTransform.localPosition += new Vector3(6, 10, 0);
+            text.rectTransform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+            text.text = "I will trade";
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+
+            GameObject buyingGO = new GameObject("Buying");
+            Image image = buyingGO.AddComponent<Image>();
+            image.rectTransform.SetParent(display.transform);
+            image.rectTransform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+            image.rectTransform.localPosition += new Vector3(-16, 0, 0);
+            image.overrideSprite = booth.buying.icon;
+
+
+            GameObject forTextGO = new GameObject("Space Text");
+            text = forTextGO.AddComponent<Text>();
+            text.rectTransform.SetParent(display.transform);
+            text.rectTransform.localPosition += new Vector3(24, -16, 0);
+            text.rectTransform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
+            text.text = "for";
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+            GameObject sellingGO = new GameObject("Selling");
+            image = sellingGO.AddComponent<Image>();
+            image.rectTransform.SetParent(display.transform);
+            image.rectTransform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+            image.rectTransform.localPosition += new Vector3(16, 0, 0);
+            image.overrideSprite = booth.selling.icon;
+
+
+            display.transform.position = this.transform.position;
         }
 
-        display = new GameObject("Booth Text");
-        display.transform.parent = GameObject.Find("UI").transform.GetChild(1);
-        display.transform.localScale = new Vector3(1, 1, 1);
-
-
-        GameObject textGO = new GameObject("Title Text");
-        Text text = textGO.AddComponent<Text>();
-        text.rectTransform.SetParent(display.transform);
-        text.rectTransform.localPosition += new Vector3(6, 10, 0);
-        text.rectTransform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
-        text.text = "I will trade";
-        text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-
-
-        GameObject buyingGO = new GameObject("Buying");
-        Image image = buyingGO.AddComponent<Image>();
-        image.rectTransform.SetParent(display.transform);
-        image.rectTransform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-        image.rectTransform.localPosition += new Vector3(-16, 0, 0);
-        image.overrideSprite = booth.buying.icon;
-
-
-        GameObject forTextGO = new GameObject("Space Text");
-        text = forTextGO.AddComponent<Text>();
-        text.rectTransform.SetParent(display.transform);
-        text.rectTransform.localPosition += new Vector3(24, -16, 0);
-        text.rectTransform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
-        text.text = "for";
-        text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-
-        GameObject sellingGO = new GameObject("Selling");
-        image = sellingGO.AddComponent<Image>();
-        image.rectTransform.SetParent(display.transform);
-        image.rectTransform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-        image.rectTransform.localPosition += new Vector3(16, 0, 0);
-        image.overrideSprite = booth.selling.icon;
-
-
-        display.transform.position = this.transform.position;
+        
     }
 
     void OnTriggerExit2D(Collider2D collision)
